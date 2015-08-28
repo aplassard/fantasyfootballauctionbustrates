145e818fc0c06b0e9a6db780e9a2df9b470c1754
aplassard <andrew.plassard@gmail.com>
2015-08-28 08:51:47 -0500

    Updates to main and Utils to parse auction costs, bust rates, and end of season rank weights

--- ffab/Utils.py
+++ ffab/Utils.py
@@ -1,7 +1,7 @@
 '''Useful utilities for the FFAB'''
 import numpy as np
 
-def parse_file(file_path, rank_column, cost_column, cost_func):
+def parse_value_file(file_path, rank_column, cost_column, cost_func):
     '''Parses a player value file'''
     file_reader = open(file_path, 'r')
     file_reader.readline() # assuming there is a header. Yolo
@@ -22,3 +22,14 @@ def parse_file(file_path, rank_column, cost_column, cost_func):
     for ke in d_tmp.keys():
         tier_values[ke] = int(cost_func(d_tmp[ke]))
     return tier_values
+
+def parse_bust_file(file_path):
+    '''Parses a bust rate file in the specified format'''
+    f_reader = open(file_path, 'r')
+    n=0
+    bust_d = {}
+    for line in f_reader:
+        line = line.strip().split(",")
+        n += 1
+        bust_d[n] = [float(x)/100 for x in line[1:]]
+    return bust_d
--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def parse_args():
     parser.add_argument('--rb-rank-column',
                         help='Column in RB csv file containing RB rank (default 6)',
                         type=int, required=False, default=6)
+    parser.add_argument('--rb-bust-rates', required=True, help='RB Bust rate file')
     parser.add_argument('--wr-auction-file',
                         help='CSV file containing wide receiver auction values (required)',
                         type=str, required=True)
@@ -41,29 +42,55 @@ def parse_args():
     parser.add_argument('--wr-rank-column',
                         help='Column in WR csv file containing WR rank (default 5)',
                         type=int, required=False, default=5)
+    parser.add_argument('--wr-bust-rates', required=True, help='WR Bust rate file (required)')
     parser.add_argument('--qb-te-d-k-cost', help='Total cost spent on QB, TE, D, and K (default 4)',
                         type=int, required=False, default=4)
     parser.add_argument('--total-auction-cost',
                         help='Total money available for auction (default 200)',
                         type=int, required=False, default=200)
     parser.add_argument('--position-value-function',
-                        help='Function to calculate auction cost by position (mean, median, max, min)',
+                        help='Function to calculate auction position cost (mean, median, max, min)',
                         type=str, required=False, default='median')
+    parser.add_argument('--num-draft-picks', help='Maximum number of picks to make (default 11)',
+                        type=int, required=False, default=11)
+    parser.add_argument('--num-teams', help='Number of random teams to build (default 1000)',
+                        type=int, required=False, default=1000)
+    parser.add_argument('--rb-weights', help='Weight for RBs by end tier (default 1,.5,.25,0,0,0)',
+                        type=str, required=False, default='1,.5,.25,0,0,0')
+    parser.add_argument('--wr-weights', help='Weight for WRs by end tier (default 1,.5,.25,0,0,0)',
+                        type=str, required=False, default='1,.5,.25,0,0,0')
     return parser.parse_args()
 
 def main():
     '''Runs the main function for this script'''
     args = parse_args()
+
     rb_value_file = os.path.abspath(args.rb_auction_file)
     wr_value_file = os.path.abspath(args.wr_auction_file)
+
+    rb_bust_file = os.path.abspath(args.rb_bust_rates)
+    wr_bust_file = os.path.abspath(args.wr_bust_rates)
+
     rb_rank_column = args.rb_rank_column
     rb_cost_column = args.rb_cost_column
     wr_rank_column = args.wr_rank_column
     wr_cost_column = args.wr_cost_column
-    cost_function = parse_position_function(args.position_value_function)
 
-    rb_tiers = Utils.parse_file(rb_value_file, rb_rank_column, rb_cost_column, cost_function)
-    wr_tiers = Utils.parse_file(wr_value_file, wr_rank_column, wr_cost_column, cost_function)
+    num_picks = args.num_draft_picks
+    num_teams = args.num_teams
+    available_cost = args.total_auction_cost - args.qb_te_d_k_cost
+
+    rb_weights = map(float, args.rb_weights.split(","))
+    wr_weights = map(float, args.wr_weights.split(","))
+
+    cost_function = parse_position_function(args.position_value_function)
+    rb_tiers = Utils.parse_value_file(rb_value_file, rb_rank_column, rb_cost_column, cost_function)
+    wr_tiers = Utils.parse_value_file(wr_value_file, wr_rank_column, wr_cost_column, cost_function)
+
+    rb_busts = Utils.parse_bust_file(rb_bust_file)
+    wr_busts = Utils.parse_bust_file(wr_bust_file)
+
+    team_simulator = TeamSimulator()
 
 if __name__ == '__main__':
     main()
